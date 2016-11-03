fa1ffc26421f1b77a037f5ec2c716599f91f195c
dennis <dennis.seiffert@web.de>
2016-11-03 21:57:53 +0100

    remove dead code

--- source/Web/elevation.js
+++ source/Web/elevation.js
@@ -1,36 +1,8 @@
 // Load the Visualization API and the columnchart package.
 google.load('visualization', '1', {packages: ['columnchart']});
 
-// function initMap() {
-//   // The following path marks a path from Mt. Whitney, the highest point in the
-//   // continental United States to Badwater, Death Valley, the lowest point.
-//   var path = [
-//       {lat: 36.579, lng: -118.292},  // Mt. Whitney
-//       {lat: 36.606, lng: -118.0638},  // Lone Pine
-//       {lat: 36.433, lng: -117.951},  // Owens Lake
-//       {lat: 36.588, lng: -116.943},  // Beatty Junction
-//       {lat: 36.34, lng: -117.468},  // Panama Mint Springs
-//       {lat: 36.24, lng: -116.832}];  // Badwater, Death Valley
-
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 8,
-//     center: path[1],
-//     mapTypeId: 'terrain'
-//   });  
-
-//   // Draw the path, using the Visualization API and the Elevation service.
-//   displayPathElevation(path, elevator, map);
-// }
 
 function displayPathElevation(path, map) {
-//   // Display a polyline of the elevation path.
-//   new google.maps.Polyline({
-//     path: path,
-//     strokeColor: '#0000CC',
-//     opacity: 0.4,
-//     map: map
-//   });
-
   // Create an ElevationService.
   var elevator = new google.maps.ElevationService;
 
